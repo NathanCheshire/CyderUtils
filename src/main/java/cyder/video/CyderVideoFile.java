8757fcb86f189b9b88650955c3e2da6f236fd5c8
Nathan Cheshire <nathan.vincent.2.718@gmail.com>
2023-11-23 10:53:14 -0600

    adding javadoc and constructor to cydervideofile

--- src/main/java/cyder/video/CyderVideoFile.java
+++ src/main/java/cyder/video/CyderVideoFile.java
@@ -5,14 +5,28 @@ import cyder.audio.validation.SupportedAudioFileType;
 
 import java.io.File;
 
+/**
+ * An encapsulated supported video file for performing operations on.
+ * See {@link SupportedVideoFormat} for the formats possible for encapsulation.
+ */
 public final class CyderVideoFile {
+    /**
+     * The encapsulated video file.
+     */
     private final File videoFile;
 
+    /**
+     * Constructs a new CyderVideoFileObject.
+     *
+     * @param videoFile the video file
+     * @throws NullPointerException     if the provided file is null
+     * @throws IllegalArgumentException if the provided file does not exist, is not a file, or is not a supported type
+     */
     public CyderVideoFile(File videoFile) {
         Preconditions.checkNotNull(videoFile);
         Preconditions.checkArgument(videoFile.exists());
         Preconditions.checkArgument(videoFile.isFile());
-        Preconditions.checkArgument();
+        Preconditions.checkArgument(SupportedVideoFormat.isSupportedVideoFormat(videoFile));
 
         this.videoFile = videoFile;
     }
