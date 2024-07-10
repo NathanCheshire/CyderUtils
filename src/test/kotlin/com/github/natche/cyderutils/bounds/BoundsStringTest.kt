fe132c339e7ec6ba8a6ae2b42a968d7cd5e574db
Nathan Cheshire <nathan.vincent.2.718@gmail.com>
2024-07-09 21:30:31 -0500

    fixing tests

--- src/test/kotlin/com/github/natche/cyderutils/bounds/BoundsStringTest.kt
+++ src/test/kotlin/com/github/natche/cyderutils/bounds/BoundsStringTest.kt
@@ -1,11 +1,7 @@
 package com.github.natche.cyderutils.bounds
 
-import com.github.natche.cyderutils.files.FileUtil
 import com.github.natche.cyderutils.font.CyderFonts
-import com.github.natche.cyderutils.subroutines.NecessarySubroutines
-import com.github.natche.cyderutils.utils.ArrayUtil
 import com.github.natche.cyderutils.utils.OsUtil
-import com.github.natche.cyderutils.utils.StaticUtil
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.BeforeAll
@@ -51,42 +47,45 @@ class BoundsStringTest {
             .setMaxWidth(400)
             .build()
 
-        assertEquals(363.04296875, bs.width)
-        assertEquals(56.8203125, bs.height)
-        assertEquals("<html>Some text without HTML styling<br/>and even more text after that</html>", bs.text)
+        assertEquals(380.8212890625, bs.width)
+        assertEquals(57.099609375, bs.height)
+        assertEquals("<html>Some text without HTML styling"
+                + " and even more text<br/>after that</html>", bs.text)
 
-        bs = BoundsString.Builder("The quick brown fox jumps over the lazy dogs"
-                + " The quick brown fox jumps over the lazy dogs"
-                + " The quick brown fox jumps over the lazy dogs"
-                + " The quick brown fox jumps over the lazy dogs")
+        bs = BoundsString.Builder(
+            "The quick brown fox jumps over the lazy dogs"
+                    + " The quick brown fox jumps over the lazy dogs"
+                    + " The quick brown fox jumps over the lazy dogs"
+                    + " The quick brown fox jumps over the lazy dogs"
+        )
             .setFont(CyderFonts.DEFAULT_FONT_SMALL)
             .setMaxWidth(400)
             .build()
 
-        assertEquals(380.05859375, bs.width)
-        assertEquals(180.4609375, bs.height)
-        assertEquals("<html>The quick brown fox jumps over<br/>"
-                + "the lazy dogs The quick brown<br/>"
-                + "fox jumps over the lazy dogs The<br/>"
-                + "quick brown fox jumps over the<br/>"
-                + "lazy dogs The quick brown fox<br/>"
-                + "jumps over the lazy dogs</html>", bs.text)
+        assertEquals(389.125, bs.width)
+        assertEquals(119.19921875, bs.height)
+        assertEquals(
+            "<html>The quick brown fox jumps over the lazy dogs The<br/>"
+                    + "quick brown fox jumps over the lazy dogs The quick<br/>"
+                    + "brown fox jumps over the lazy dogs The quick brown<br/>"
+                    + "fox jumps over the lazy dogs</html>", bs.text
+        )
 
-        bs = BoundsString.Builder("The quick brown fox jumps over the lazy dogs"
-                + " The quick brown fox jumps over the lazy dogs"
-                + " The quick brown fox jumps over the lazy dogs"
-                + " The quick brown fox jumps over the lazy dogs")
+        bs = BoundsString.Builder(
+            "The quick brown fox jumps over the lazy dogs"
+                    + " The quick brown fox jumps over the lazy dogs"
+                    + " The quick brown fox jumps over the lazy dogs"
+                    + " The quick brown fox jumps over the lazy dogs"
+        )
             .setFont(CyderFonts.DEFAULT_FONT_LARGE)
             .setMaxWidth(800)
             .build()
 
-        assertEquals(771.4697265625, bs.width)
-        assertEquals(226.103515625, bs.height)
-        assertEquals("<html>The quick brown fox jumps over the lazy<br/>"
-                + "dogs The quick brown fox jumps over the<br/>"
-                + "lazy dogs The quick brown fox jumps over<br/>"
-                + "the lazy dogs The quick brown fox jumps<br/>"
-                + "over the lazy dogs</html>", bs.text)
+        assertEquals(780.40771484375, bs.width)
+        assertEquals(134.32861328125, bs.height)
+        assertEquals("<html>The quick brown fox jumps over the lazy dogs The quick brown fox<br/>"
+                    + "jumps over the lazy dogs The quick brown fox jumps over the lazy<br/>"
+                    + "dogs The quick brown fox jumps over the lazy dogs</html>", bs.text)
     }
 
     /**
@@ -94,22 +93,25 @@ class BoundsStringTest {
      */
     @Test
     fun testWithHtmlStyling() {
-        val bs = BoundsString.Builder("<html>Some text with HTML<br/>"
-                + " styling and even <div>more</div> text after that</html>")
+        val bs = BoundsString.Builder(
+            "<html>Some text with HTML<br/>"
+                    + " styling and even <div>more</div> text after that</html>"
+        )
             .setFont(CyderFonts.DEFAULT_FONT_SMALL)
             .setMaxWidth(400)
             .build()
 
-        assertEquals(240.92578125, bs.width)
-        assertEquals(87.73046875, bs.height)
-        assertEquals("<html>Some text with HTML<br/> styling"
-                + " and even<br/><div>more</div> text after that</html>", bs.text)
+        assertEquals(279.8662109375, bs.width)
+        assertEquals(57.099609375, bs.height)
+        assertEquals("<html>Some text with HTML<br/>"
+                + " styling and even <div>more</div> text after that</html>", bs.text
+        )
     }
 
     companion object {
         @JvmStatic
         @BeforeAll
-        fun beforeAll(): Unit {
+        fun beforeAll() {
             val fontFiles = OsUtil.buildFile("static", "fonts").listFiles()
             val ge = GraphicsEnvironment.getLocalGraphicsEnvironment()
 
