11f4976e6f9b1cf7363e3db1555868742b44dcee
Nathan Cheshire <nathan.vincent.2.718@gmail.com>
2024-09-12 22:14:58 -0500

    update and correction

--- src/main/java/com/github/natche/cyderutils/bounds/BoundsComputationException.java
+++ src/main/java/com/github/natche/cyderutils/bounds/BoundsComputationException.java
@@ -3,7 +3,7 @@ package com.github.natche.cyderutils.bounds;
 import com.github.natche.cyderutils.exceptions.CyderException;
 
 /** An exception thrown by {@link BoundsString}s. */
-public class BoundsComputationException extends CyderException {
+public final class BoundsComputationException extends CyderException {
     public BoundsComputationException(String errorMessage) {
         super(errorMessage);
     }
